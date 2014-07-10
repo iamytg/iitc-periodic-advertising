3aafe2d806dffe156a2807dd15363b9cde9c6563
iamytg <iamytg@gmail.com>
2014-07-10 16:09:54 +0900

    메시지 첫 실행 추가

--- iitc-periodic-advertising.js
+++ iitc-periodic-advertising.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       IRK 광고 메시지 스케줄러
 // @namespace  https://github.com/iamytg/iitc-periodic-advertising
-// @version    0.1
+// @version    0.2
 // @description  IRK 광고를 정기적으로 띄워줍니다.
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
@@ -18,10 +18,18 @@ $(function() {
         return $(this).text().toLowerCase() == "faction";
     });
     var chattext = $("#chattext");
-    
-    setInterval(function() {
+    var post = function() {
         chat.chooseAnchor(tabFaction);
         chattext.val(msg);
         chat.postMsg();
-    }, intervalMinutes * 1000 * 60); // 최소한 IITC 화면접속 최초 구성시간 3초보다 커야함.
+    };
+    
+    var timer = setInterval(function () {
+        if(chat && chat.postMsg) {
+            clearInterval(timer);
+            //console.log("메시지 첫 실행!");
+            post();
+        } else console.log("메시지 첫 실행을 위해 대기중");
+    }, 1000);
+    setInterval(post, intervalMinutes * 1000 * 60); // 최소한 IITC 화면접속 최초 구성시간 3초보다 커야함.
 });
